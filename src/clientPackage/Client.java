f302f1563070eaef423991e6329eb462c576fc9e
lajmia69 <lajmiahmed175@gmail.com>
2025-09-29 22:46:25 +0100

    1
    1

--- src/clientPackage/Client.java
+++ src/clientPackage/Client.java
@@ -5,18 +5,18 @@ import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) {
-        String host = "127.0.0.1"; // ou remplacer par une adresse IP distante
+        String host = "127.0.0.1"; 
         int port = 1234;
 
         try {
-            // Première étape : avant connexion
+           
             System.out.println("Je suis un client pas encore connecté...");
 
-            // Tentative de connexion
+      
             try (Socket socket = new Socket(host, port)) {
                 System.out.println("Je suis un client connecté !");
                 System.out.println("Adresse locale du socket : " + socket.getLocalSocketAddress());
-                // Dernière étape : le socket sera fermé automatiquement
+          
             }
 
         } catch (IOException e) {
