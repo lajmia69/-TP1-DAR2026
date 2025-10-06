3b63f10a47e1182ab7d7aadf7b8df72a95c4a16d
lajmia69 <lajmiahmed175@gmail.com>
2025-10-06 15:12:20 +0100

    a
    a

--- src/clientPackage/Client.java
+++ src/clientPackage/Client.java
@@ -1,26 +1,41 @@
 package clientPackage;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
-        String host = "127.0.0.1"; 
-        int port = 1234;
+        try (Scanner scanner = new Scanner(System.in)) {
+            String host = "127.0.0.1";
+            int port = 1234;
 
-        try {
-           
-            System.out.println("Je suis un client pas encore connecté...");
+            try {
+                System.out.println("Je suis un client pas encore connecté...");
 
-      
-            try (Socket socket = new Socket(host, port)) {
-                System.out.println("Je suis un client connecté !");
-                System.out.println("Adresse locale du socket : " + socket.getLocalSocketAddress());
-          
+                try (Socket socket = new Socket(host, port)) {
+                    System.out.println("Je suis un client connecté !");
+                    System.out.println("Adresse locale du socket : " + socket.getLocalSocketAddress());
+                    System.out.println("donner un nombre");
+                    int number = scanner.nextInt();
+                    OutputStream os = socket.getOutputStream();
+                    DataOutputStream dos = new DataOutputStream(os);
+                    dos.writeInt(number);
+                    System.out.println("element envoyee");
+                    System.out.println("je vais recevoir le resultat de la multiplication par 5");
+                     InputStream is = socket.getInputStream();
+                     DataInputStream dis = new DataInputStream(is);
+                     int recu = dis.readInt();
+
+                }
+
+            } catch (IOException e) {
+                System.err.println("Erreur lors de la connexion : " + e.getMessage());
             }
-
-        } catch (IOException e) {
-            System.err.println("Erreur lors de la connexion : " + e.getMessage());
         }
     }
 }
--- src/serverPackage/server.java
+++ src/serverPackage/server.java
@@ -1,6 +1,10 @@
 package serverPackage;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,6 +16,17 @@ public class Server {
 
             try (Socket clientSocket = serverSocket.accept()) {
                 System.out.println("Un client est connecté ! Adresse : " + clientSocket.getInetAddress());
+                  InputStream is = clientSocket.getInputStream();
+                  DataInputStream dis = new DataInputStream(is);
+                  int number = dis.readInt();
+                  System.out.println("Le nombre reçu est : " + number);
+                   OutputStream os = clientSocket.getOutputStream();
+                    DataOutputStream dos = new DataOutputStream(os);
+                    dos.writeInt(number);
+                  System.out.println("je vais rentrer a vous la multiplication par 5");
+                    int result = number * 5;
+                    dos.writeInt(result);
+                    System.out.println("Le résultat a été envoyé au client.");
             }
 
         } catch (IOException e) {
